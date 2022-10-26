4bb2397c390bb9facf98f1842ad9cb43bcbb21d7
Chaehyeon <chaehyeon119>
2022-10-26 12:43:44 +0900

    django-environ 활용한 환경변수 로딩

--- app/management/commands/send_hello_email.py
+++ app/management/commands/send_hello_email.py
@@ -1,9 +1,34 @@
-from django.core.management import BaseCommand
+from typing import List
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
+from django.core.management import BaseCommand, CommandError
+from django.core.validators import validate_email
+from django.template.loader import render_to_string
 
 
 class Command(BaseCommand):
-    # def add_arguments(self, parser):
-    #    super().add_arguments(parser)
+    def add_arguments(self, parser):
+        parser.add_argument("receiver", nargs="+", type=str, help=">dz!®–|!õ—")
 
     def handle(self, *args, **options):
-        print("hello world")
+        subject = render_to_string("app/hello_email_subject.txt")
+        content = render_to_string(
+            "app/hello_email_content.txt", {"event_name": "파이썬 사랑방 이벤트"}
+        )
+        sender_email = settings.DEFAULT_FROM_EMAIL
+        receiver_email_list: List[str] = options["receiver"]
+
+        try:
+            for email in receiver_email_list:
+                validate_email(email)
+        except ValidationError as e:
+            raise CommandError(e.message)
+
+        send_mail(
+            subject,
+            content,
+            sender_email,
+            receiver_email_list,
+            fail_silently=False,
+        )
